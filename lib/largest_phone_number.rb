ee4aa0d55a73d5a4e0c8faa7c91fec1b29167591
Umer <myemailum14@gmail.com>
2015-11-15 17:56:22 -0500

    awesome solution

--- lib/largest_phone_number.rb
+++ lib/largest_phone_number.rb
@@ -2,22 +2,11 @@ require 'pry'
 # your #largest_phone_number method goes here
 
 def largest_phone_number(arr)
-	if (arr.size == 0 || !arr.is_a?(Array))
-		raise "error"
-		return
-	end
-	data = arr.map { |x| [x.scan(/\d/).map(&:to_i).inject(:+), x] }
+	raise "error" if (arr.size == 0 || !arr.is_a?(Array))
 
-	data.sort_by! { |d|  
-		d[0]
-	}
-	binding.pry
-
-	data.size.downto(1) { |i|
-		next if (i>(data.size-1) )
-		break if (data[i][0] != data[i-1][0])
-		data.pop
-	}
-
-	data[-1][1]
+	arr.sort! { |a,b| 
+		sum1 = a.scan(/\d/).map(&:to_i).inject(:+)
+		sum2 = b.scan(/\d/).map(&:to_i).inject(:+)
+		(sum1 <=> sum2) == 0 ? -1 : (sum1 <=> sum2)
+	}[-1]
 end
