d7eba04a2a37d532074bc9f3c371ab5aacbdc24b
Giancarlo Martinez <g.r.rmartinez49@gmail.com>
2015-11-28 15:01:28 -0500

    add largest phone number function. final commit

--- lib/largest_phone_number.rb
+++ lib/largest_phone_number.rb
@@ -1,1 +1,25 @@
 # your #largest_phone_number method goes here
+
+
+def largest_phone_number(numbers)
+	if numbers.empty? || numbers.class != Array 
+		raise "hi there, these aren't arrays"
+	else
+		largest = "0"	
+		numbers.each do |num|
+			if phone_sum(num) >= phone_sum(largest)
+				largest = num
+			end
+		end
+		return largest
+	end
+end
+
+def phone_sum(phone_num)
+	sum = 0
+	nums = phone_num.gsub(/[()-]+/, "").split("")
+	nums.each do |num|
+	   sum += num.to_i
+	end 
+	return sum
+end
