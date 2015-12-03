41fc4b6c8e0be893cb425e48727529c5ae6f2712
github username <mendelblesofsky@gmail.com>
2015-12-03 15:28:31 -0500

    shorten

--- lib/largest_phone_number.rb
+++ lib/largest_phone_number.rb
@@ -1,6 +1,6 @@
 # your #largest_phone_number method goes here
 def largest_phone_number(arr)
 	raise 'argument must be an array containing some data' if (arr.empty? || !arr.is_a?(Array))
-	sum = arr.map { |a| a.gsub(/[\(\-)]/, '').split('').map(&:to_i).inject { |total, num| total += num} }
+	sum = arr.map { |a| a.scan(/[^\(\)\-]/).map(&:to_i).inject(:+) }
 	arr[sum.rindex(sum.max)]
 end
