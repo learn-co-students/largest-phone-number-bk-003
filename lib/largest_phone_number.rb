9b334012bc477c7a97a75b8dd24633b2efc56688
Peter Han <han.peter92@gmail.com>
2015-12-08 14:07:22 -0500

    done

--- lib/largest_phone_number.rb
+++ lib/largest_phone_number.rb
@@ -1,1 +1,13 @@
 # your #largest_phone_number method goes here
+def largest_phone_number(array)
+	clone = array.clone
+	sum = []
+	clone.each do |num|
+		numbers = []
+		num.split("").each do |e|
+			numbers << e.to_i
+		end
+		sum << numbers.inject(:+)
+	end
+	array[sum.each_index.select{|i| sum[i] == sum.max}.last]
+end
